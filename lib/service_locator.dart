1f71116f75640c99bbe992907138b75b6e93c60c
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-01-22 17:41:00 +0200

    create app navigator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,6 +4,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 final GetIt serviceLocator = GetIt.instance;
 
 class ServiceLocator {
+  Future<void> setupServiceLocator() async {
+    await _setupForExternal();
+  }
+
   Future<void> _setupForExternal() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
 
