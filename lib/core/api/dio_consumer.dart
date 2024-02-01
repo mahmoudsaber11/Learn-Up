48a7a109c865bb0f0bcf962e2941814ca4d235a4
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-02-01 03:10:08 +0200

    finish dio consumer

--- lib/core/api/dio_consumer.dart
+++ lib/core/api/dio_consumer.dart
@@ -1,17 +1,53 @@
 import 'package:dio/dio.dart';
+import 'package:learn_up/core/api/api_consumer.dart';
 
-class DioConsumer {
+class DioConsumer implements ApiConsumer {
   final Dio dio;
 
   DioConsumer({required this.dio});
+  @override
+  Future delete(String path,
+      {Map<String, dynamic>? queryParameters,
+      Map<String, dynamic>? data}) async {
+    final Response response =
+        await dio.delete(path, queryParameters: queryParameters, data: data);
+    return response.data;
+  }
 
-  Future<dynamic> post(
-    String path, {
-    Map<String, dynamic>? queryParameters,
-    Map<String, dynamic>? data,
-  }) async {
+  @override
+  Future get(String path, {Map<String, dynamic>? queryParameters}) async {
+    final Response response =
+        await dio.get(path, queryParameters: queryParameters);
+    return response.data;
+  }
+
+  @override
+  Future patch(String path,
+      {Map<String, dynamic>? queryParameters,
+      Map<String, dynamic>? data}) async {
+    final Response response =
+        await dio.patch(path, queryParameters: queryParameters, data: data);
+    return response.data;
+  }
+
+  @override
+  Future post(String path,
+      {Map<String, dynamic>? queryParameters,
+      Map<String, dynamic>? data}) async {
     final Response response =
         await dio.post(path, queryParameters: queryParameters, data: data);
-    return response;
+    return response.data;
+  }
+
+  @override
+  Future put(String path,
+      {Map<String, dynamic>? queryParameters,
+      Map<String, dynamic>? data}) async {
+    final Response response = await dio.put(
+      path,
+      queryParameters: queryParameters,
+      data: data,
+    );
+    return response.data;
   }
 }
