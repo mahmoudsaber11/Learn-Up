fff598420dc880c1b7d08083f868d7c58cf52efc
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-02-01 02:59:43 +0200

    create api consumer

---
+++ lib/core/api/api_consumer.dart
@@ -0,0 +1,30 @@
+abstract class ApiConsumer {
+  Future<dynamic> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+  });
+
+  Future<dynamic> post(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? data,
+  });
+
+  Future<dynamic> patch(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? data,
+  });
+
+  Future<dynamic> put(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? data,
+  });
+
+  Future<dynamic> delete(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? data,
+  });
+}
